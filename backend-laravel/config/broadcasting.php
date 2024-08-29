a20df69f594d7c3d96cacff17ae307bccd130ae8
m4rkbello <markbello10041999@gmail.com>
2024-08-29 23:41:29 +0800

    TUGPA

---
+++ backend-laravel/config/broadcasting.php
@@ -0,0 +1,40 @@
+<?php
+
+return [
+
+    'default' => env('BROADCAST_DRIVER', 'log'),
+
+    'connections' => [
+
+        'pusher' => [
+            'driver' => 'pusher',
+            'key' => env('2ff698da650ee6a10d84'),
+            'secret' => env('178877667d60c9a55ea4'),
+            'app_id' => env('1857216'),
+            'options' => [
+                'cluster' => env('ap1'),
+                'useTLS' => true,
+            ],
+        ],
+
+        'ably' => [
+            'driver' => 'ably',
+            'key' => env('ABLY_KEY'),
+        ],
+
+        'redis' => [
+            'driver' => 'redis',
+            'connection' => 'default',
+        ],
+
+        'log' => [
+            'driver' => 'log',
+        ],
+
+        'null' => [
+            'driver' => 'null',
+        ],
+
+    ],
+
+];
