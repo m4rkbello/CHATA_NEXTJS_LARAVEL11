e61410b9e5ceca154f33a9c9dbb8c86d050512ad
m4rkbello <markbello10041999@gmail.com>
2024-09-19 21:46:46 +0800

    FRONTEND: ADD NEXTJS MODULE EXPORT

--- frontend-nextjs/tailwind.config.js
+++ frontend-nextjs/tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './node_modules/@shadcn/ui/**/*.js', // Add this line
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
