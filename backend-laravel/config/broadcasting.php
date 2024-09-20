67b9d2baf5f410e7082353dde6e4b906a8f58cae
m4rkbello <markbello10041999@gmail.com>
2024-09-20 23:57:29 +0800

    BROADCASTING: ADD AMBOT UNSA NI

--- backend-laravel/config/broadcasting.php
+++ backend-laravel/config/broadcasting.php
@@ -6,16 +6,16 @@ return [
 
     'connections' => [
 
-        'pusher' => [
-            'driver' => 'pusher',
-            'key' => env('PUSHER_APP_KEY'),
-            'secret' => env('PUSHER_APP_SECRET'),
-            'app_id' => env('PUSHER_APP_ID'),
-            'options' => [
-                'cluster' => env('PUSHER_APP_CLUSTER'),
-                'useTLS' => true,
-            ],
-        ],
+        // 'pusher' => [
+        //     'driver' => 'pusher',
+        //     'key' => env('PUSHER_APP_KEY'),
+        //     'secret' => env('PUSHER_APP_SECRET'),
+        //     'app_id' => env('PUSHER_APP_ID'),
+        //     'options' => [
+        //         'cluster' => env('PUSHER_APP_CLUSTER'),
+        //         'useTLS' => true,
+        //     ],
+        // ],
     
         'reverb' => [
             'driver' => 'reverb',
